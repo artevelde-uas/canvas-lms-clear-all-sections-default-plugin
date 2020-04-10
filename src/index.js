75e195b7f84b54f424322f20252f5f318674c76c
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-04-10 13:01:24 +0200

    Rename plug-in

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+export default function ({ router, dom }) {
+    router.addListener('courses.announcements.new', params => {
+
+        dom.onElementAdded('input[type="hidden"][name="specific_sections"]', specificSections => {
+            let selector = '[name="specific_sections"] + span > span > span > span:first-child + span > span > span > button';
+            let allSectionsButton = specificSections.parentElement.querySelector(selector);
+            let discussionTitle = document.getElementById('discussion-title');
+
+            if (allSectionsButton === null) return;
+
+            // Click to delete the 'All sections' button under 'Post to'
+            allSectionsButton.click();
+
+            // Set focus back on title input
+            discussionTitle.focus();
+        });
+
+    });
+}
