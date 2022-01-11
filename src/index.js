dda53c1f19758593add08dde7fb5a66688e71cce
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-01-11 22:57:00 +0100

    Add package translations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { router, dom } from '@artevelde-uas/canvas-lms-app';
 
+import __ from './i18n';
+
 
 export default function () {
 
@@ -34,4 +36,9 @@ export default function () {
         });
     });
 
+    return {
+        ...require('../package.json'),
+        title: __('package.title'),
+        description: __('package.description')
+    };
 }
